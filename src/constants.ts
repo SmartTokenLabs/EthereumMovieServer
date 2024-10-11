816ae6b76c582fddc35d567da672416b0fa341ed
James Brown <soylentblue@hotmail.co.uk>
2024-10-11 17:25:48 +0800

    Update constants

--- src/constants.ts
+++ src/constants.ts
@@ -6,3 +6,4 @@ export const SQLite_DB_FILE = process.env.SQLite_DB_FILE;
 export const PATH_TO_CERT = process.env.CERT_PATH;
 export const INFURA_KEY = process.env.INFURA_KEY;
 export const MOVIE_NAME = process.env.MOVIE_NAME;
+export const NODE_ENV = process.env.NODE_ENV;
